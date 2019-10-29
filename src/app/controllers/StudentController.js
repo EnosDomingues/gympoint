815cc6ab9aa315ccd784ab2e13c8d80bb9009bc5
dev__ <dev__@MacBook-Pro-de-Enos.local>
2019-10-29 20:11:57 -0300

    Desafio 2: Gympoint, o início - concluído

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -42,38 +42,40 @@ class StudentController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      idade: Yup.number(),
-      peso: Yup.number(),
-      altura: Yup.number(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
     });
 
-    if (!(await schema.isValid(res.body))) {
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails.' });
     }
 
-    const { email, oldPassword } = req.body;
+    const { email } = req.body;
 
-    const student = await Student.findByPk(req.userId);
+    const student = await Student.findByPk(req.body.id);
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists.' });
+    }
 
     if (email !== student.email) {
       const studentExists = await Student.findOne({ where: { email } });
 
       if (studentExists) {
-        return res.status(400).json({ error: 'User already exists.' });
+        return res.status(400).json({ error: 'Email already exists.' });
       }
     }
 
-    if (oldPassword && !(await student.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match.' });
-    }
-
-    const { id, name, admin } = await student.update(req.body);
+    const { id, name, age, weight, height } = await student.update(req.body);
 
     return res.json({
       id,
       name,
       email,
-      admin,
+      age,
+      weight,
+      height,
     });
   }
 }
